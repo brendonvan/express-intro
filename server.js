bf5eb788d95ef3dce6e6a037f36de72b5f8b6279
brendonvan <brendon@van.digital>
2022-08-15 10:45:38 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+
+// in server.js
+
+// Require modules
+const express = require('express');
+	
+// Create the Express app
+const app = express();
+	
+// Configure the app (app.set)
+
+	
+// Mount middleware (app.use)
+	
+	
+// Mount routes
+// app.get('/:phil', function(req, res) {
+//     res.send(`<h1>${req.params.phil}</h1>`);
+// });
+	
+app.get('/cars', function(req, res) {
+    res.send("Here's a list of my cars...");
+});
+
+app.post('/cars', function(req, res) {
+    res.send('Thanks for the new car!');
+});
+
+
+let name = "Brendon222"
+
+app.get('/about-me', function(req, res) {
+    res.send(`<h1>Hello World, I am ${name}!</h1>`);
+});
+
+
+
+// Tell the app to listen on port 4000
+app.listen(4000, function() {
+    console.log('Listening on port 4000');
+});
